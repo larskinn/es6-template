d36c01582fc5dff7ff7e393bdfe15657dbef6229
Lars Kinn Ekroll <lars.kinn.ekroll@bekk.no>
2016-01-31 17:48:41 +0100

    Emacs project-specific settings

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+(
+ (js2-mode . ((js2-basic-offset . 2)
+              (indent-tabs-mode . nil)))
+ (js-mode . ((js-indent-level . 2)))
+ (web-mode . ((web-mode-markup-indent-offset . 2)
+              (web-mode-css-indent-offset . 2))))
